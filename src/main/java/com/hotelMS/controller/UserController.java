ac2e642c127b4e7f2027e74b0039f3c093371491
accesstech01 <56120293+accesstech01@users.noreply.github.com>
2020-09-24 22:19:31 +0100

    update and resolve the error

--- src/main/java/com/hotelMS/controller/UserController.java
+++ src/main/java/com/hotelMS/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
+
 public class UserController {
 
     @Autowired
--- src/main/java/com/hotelMS/repository/UserRepository.java
+++ src/main/java/com/hotelMS/repository/UserRepository.java
@@ -14,6 +14,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsername(String username);
 
+    @Query("SELECT u FROM User u WHERE u.username = ?1 and u.password =?2")
     Optional<User> loginUserDetails(String username, String password);
 
     User findByEmail(String email);
